b317170456203725f7d21574ed929be04985737c
Марсель <marsenhub@gmail.com>
2025-12-19 00:38:57 +0300

    Проверка на палиндром

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 print("Hello world")
+word = input("Введите слово: ")
+wordLower = word.lower() 
+firstIndex = 0
+lastIndex = len(word) - 1
+tf = False
+for c in wordLower:
+    if(wordLower[firstIndex] == wordLower[lastIndex]):
+        tf = True
+    else:
+        tf = False
+        break
+    firstIndex += 1
+    lastIndex -= 1
+
+if(tf): print(f"Word {word} is palindrome")
+else: print(f"Word {word} is not palindrome")
