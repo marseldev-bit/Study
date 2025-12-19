701f60de8fb85639b267c2765d3e73f57a97fc93
Марсель <marsenhub@gmail.com>
2025-12-19 14:20:45 +0300

    Удаление повторяющихся элементов массива

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 print("Hello world")
-arr = [2, 15, 4, 90, 7, 10, 1, 0]
+arr = [10, 2, 10, 10, 10, 3, 4, 10, 4, 5, 19, 0]
 
-for i in arr:
-    temp = 0
-    index = 0
-    for j in range(len(arr)-1):
-        if(arr[index] > arr[index+1]):
-            temp = arr[index]
-            arr[index] = arr[index+1]
-            arr[index+1] = temp
-        index+=1
+index = 0
+
+while(index < len(arr)):
+    temp = 1
+    while((index+temp) < len(arr)):
+        if(arr[index] == arr[index+temp]):
+            del arr[index]
+            index-=1
+            break
+        temp+=1
+    index+=1
+
 print(arr)
