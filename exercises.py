0e6ea70bcbdc94480b8f006286f8c7bd9794010c
Марсель <marsenhub@gmail.com>
2025-12-19 19:31:08 +0300

    2. Переменные и типы данных

---
+++ exercises.py
@@ -0,0 +1,43 @@
+# 2.3
+# name = "Clementine"
+# print(f"Hi, {name}, do you want to study Python today?")
+
+#2.4 
+# name1 = "Belle jackson"
+# name2 = 'klyde Dupreeh'
+# name3 = 'mike wheeler'
+# print(f"{name1.upper()}\t{name2.lower()}\t{name3.title()}")
+
+#2.5, 2.6
+# name = "Серж Дур-Дачник"
+# quote = "Идущий дорогу осилит."
+# print(f'{name} как-то сказал одну из своих лучших цитат: "{quote}"')
+
+#2.7
+# name1 = ' Tom'
+# name2 = ' Wood '
+# name3 = 'Martin '
+# print(f"\t{name1.lstrip()}\n\t{name2.strip()}\n\t{name3.rstrip()}")
+
+#2.8
+# filename = 'pythonNotes.txt'
+# print(filename)
+# print(filename.removesuffix('.txt'))
+
+#2.9
+# print(6+2)
+# print(707-699)
+# print(0.2*40)
+# print(72/9)
+
+#2.10
+# favNumber = 4
+# message = f'My favorite number is {favNumber}'
+# print(message)
+
+#2.11
+# Работа с методами strip(), lstrip(), rstrip()
+# name1 = ' Tom'
+# name2 = ' Wood '
+# name3 = 'Martin '
+# print(f"\t{name1.lstrip()}\n\t{name2.strip()}\n\t{name3.rstrip()}")
