a2e5748857eb6d608894164b335ba9ebd91d8f67
Марсель <marsenhub@gmail.com>
2025-12-23 23:57:47 +0300

    8. Функции

--- exercises.py
+++ exercises.py
@@ -1,72 +1,139 @@
-# 7. Ввод данных и цикл while
+# 8. Функции
 
-# 7.1
-# car = input("What car would you like to drive: ")
-# print(f"Searching {car} in your city...")
+# 8.1
+# def message():
+#     print('Функции в программировании призваны выполнять определнное действие, повтоярющееся в коде несколько или более раз.')
 
-# 7.2
-# seats = int(input("How many seats do you need? "))
-# if seats <= 8: print("The table is ready")
-# else: print("Sorry, you should to wait for a free tables")
+# message()
 
-# 7.3
-# num = int(input("Enter the number: "))
-# if num % 10 == 0:
-#     print("Число кратно 10")
-# else: 
-#     print("Число делится на 10 с остатком")
+# 8.2
+# def favoriteBook(title):
+#     print(f"One of my favorite books is {title}")
 
-# 7.4
-# topping = ''
-# while True:
-#     topping = input("Enter topping to add in your order\nEnter 'quit' to end: ")
-#     if topping == 'quit': break 
-#     print(f"{topping} have been added in your order\n")
+# favoriteBook('Martin Eden')
 
-# 7.5
-# while True:
-#     age = int(input("Enter your age to get a price: "))
-#     if age < 0: break
-#     elif age >= 0 and age < 3: print("Ticket is free")
-#     elif age >= 3 and age < 13: print("Ticket is 10$")
-#     elif age > 12: print("Ticket is 15$")
+# 8.3, 8.4
+# def makeShirt(text = 'I love Python', size = 'L'):
+#     print(f"You ordered {size.upper()} size shirt with text: {text}")
 
-# 7.6
-# active = True
-# while active:
-#     message = input("Enter your age to get a price: ")
-#     if message == 'quit': 
-#         action = False
-#         break
-#     else:
-#         age = int(message)
-#         if age >= 0 and age < 3: print("Ticket is free")
-#         elif age >= 3 and age < 13: print("Ticket is 10$")
-#         elif age > 12: print("Ticket is 15$")
+# makeShirt('Stay hard', 'xl')
+# makeShirt()
 
-# 7.7 
-# while True: print(99)
+# 8.5
+# def describeCity (city, country = 'Russia'):
+#     print(f"{city} located in {country}")
 
-# 7.8, 7.9
-# sandwichOrders = ['cheeseburger', 'pastrami', 'crubsburger','pastrami', 'chickenburger', 'maestro', 'pastrami']
-# print("Pastrami isn't availible right now")
-# finishedSandwiches = []
-# for sandwich in sandwichOrders:
-#     print(f"Cooking {sandwich}...\n{sandwich} is done.\n")
-#     if sandwich != 'pastrami': finishedSandwiches.append(sandwich)
-# print("\nFinished sandwiches:")
-# for sandwich in finishedSandwiches:
-#     print(sandwich)
+# describeCity('Moscow')
+# describeCity(city='Kazan')
+# describeCity('Rome', 'Italy')
 
-# 7.10
-members = 5
-poll = {}
-while members >= 0:
-    name = input("What's your name: ")
-    poll[name] = input("If you could to visit any place in the world, what place would it be? ")
-    members -= 1
-print
-for name, place in poll.items():
-    print(f"{name} would like to visit {place}")
+# 8.6
+# def city_country(city, country):
+#     return f'{city.title()}, {country.title()}'
+
+# print(city_country('canberra', 'australia'))
+# print(city_country(country='germany', city='berlin'))
+# print(city_country(city='ottawa', country='canada'))
+
+# 8.7, 8.8
+# def makeAlbum(name, title, soundtracks=None):
+#     album = {'name': name, 'title': title}
+#     if soundtracks: album['soundtracks'] = soundtracks
+#     return album
+
+# for i in range(0, 3):
+#     musicAlbum = makeAlbum(input(), input())
+#     print(f"{musicAlbum['name']} has album '{musicAlbum['title']}'")
+
+# album = makeAlbum('Noize MC', 'Новый альбом', '21')
+# print(f"{album['name']} has album '{album['title']}' and there are {album['soundtracks']} soundtracks in it")
+
+# 8.9
+# def messages(mess):
+#     for m in mess: print(m)
+
+# message = ['Hello, Dusty!', 'I hope you fed our cat.', "I'll be late today", 'Good night!']
+# messages(message)
+
+# 8.10
+# def sendMessages(mess, smess):
+#     for i in range(len(mess)): 
+#         smess.append(mess.pop())
+#     for m in smess: print(m)
+
+# message = ['Hello, Dusty!', 'I hope you fed our cat.', "I'll be late today", 'Good night!']
+# sentMessages = []
+# sendMessages(message, sentMessages)
+# print(f'{message}\n{sentMessages}')
+
+# 8.11 (Данную задачу надо было выполнить, использовав передачу в качестве параметра среза списка, но у меня почему-то не работает)
+# def sendMessages(mess, smess):
+#     for m in mess:
+#         print(m)
+#         smess.append(m)
+
+# message = ['Hello, Dusty!', 'I hope you fed our cat.', "I'll be late today", 'Good night!']
+# sentMessages = []
+# sendMessages(message, sentMessages)
+# print(f'{message}\n{sentMessages}')
+
+# 8.12
+# def sandwich(*comp):
+#     print(f"In this sandwich we have:")
+#     for c in comp: print(f"- {c}")
+
+# sandwich('cheese', 'meat', 'pickles', 'sauce')
+
+# 8.13 (Если в **параметр такого типа передавать без ключа (например: не sex='male', а просто 'male'), то создастся кортеж, иначе создастся словарь)
+# def buildProfile(firstName, lastName, **info):
+#     info['firstName'] = firstName
+#     info['lastName'] = lastName
+#     return info
+
+# prof = buildProfile('Angel', 'Ramirez', sex='male', country='argentina')
+
+# 8.14
+# def car(brand, model, **info):
+#     info['brand'] = brand
+#     info['model'] = model
+#     return info
+
+# def showInfo(car):
+#     for key, value in car.items():
+#         if(key == 'brand'): print(f"This model of {value} named {car['model']}")
+#         elif(key == 'model'): continue
+#         else:
+#             print(value)     
+
+# honda = car('honda', 'accord', color='grey', transmission='manual transmission')
+# showInfo(honda)
+# print(honda)
+
+# 8.15
+# from modul import car
+# from modul import showInfo as info
+
+# toyota = car('toyota', 'supra', color='blue', wheel='right')
+# info(toyota)
+
+# 8.16
+# import modul
+# print(modul.car('toyota', 'supra', color='blue', wheel='right'))
+
+# from modul import car
+# print(car('toyota', 'supra', color='blue', wheel='right'))
+
+# from modul import car as c
+# print(c('toyota', 'supra', color='blue', wheel='right'))
+
+# import modul as m
+# print(m.car('toyota', 'supra', color='blue', wheel='right'))
+
+from modul import *
+print(car('toyota', 'supra', color='blue', wheel='right'))
+
+
+
+
 
 
