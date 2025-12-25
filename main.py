25abca583c982e59921d6d734ad49852442dfaf6
Марсель <marsenhub@gmail.com>
2025-12-25 16:54:35 +0300

    Запись и считывание с помощью json

--- main.py
+++ main.py
@@ -1,45 +1,19 @@
-class Car:
-    """Simple car type"""
-    def __init__(self, firm, model, year, owners):
-        self.firm = firm
-        self.model = model
-        self.year = year
-        self.run = 0
-        self.owners = owners
-    def getRunAfterOwner(self):
-        self.run = 0
-        getOwner = (self.owners[input('Run after which owner you want to get? ')])[0]
-        index = 0
-        for k, v in self.owners.items():
-            self.run += v[1]
-            index += 1
-            if index == getOwner:
-                return self.run
-            
-    def getCarName(self):
-        return(f'{self.year} {(self.firm).title()} {(self.model).title()}')
+from pathlib import Path
+import json
 
-    def getFullRun(self):
-        for v in self.owners.values():
-            self.run += v[1]
-        return self.run
-    
-    def getRunInfo(self):
-        print(f"{self.getCarName()} has {self.getFullRun()} kilometers on it")
-    
-porscheOwners = {'Jim': [1, 14000], 'Rylie': [2, 130000], 'Wincent': [3, 70000]}
-porsche = Car('porsche', 'taycan', 2023, porscheOwners)
+# Запись в файл json
+# numbers = [4, 6, 17, 8, 33, 29]
+# path = Path('files/number.json')
+# path.write_text(json.dumps(numbers))
 
-for k, v in porscheOwners.items():
-    print(f"{k.title()} was {v[0]} owner of {porsche.getCarName()} and drove {v[1]} kilometers on it")
-print()
+# Считывание из файла json
+path = Path('files/number.json')
+content = json.loads(path.read_text())
+print(content)
 
-porsche.getRunInfo()
-print()
 
-print(porsche.getRunAfterOwner())
-# porsche.run = 14000 - Первый способ присваивания значения атрибуту по умолчанию
-# porsche.giveRun(14000) - Второй способ
+
+
 
     
         
